6e76d717516d3be9cfffc7dad81d483d0512c729
jeff-杜 <1085383233@qq.com>
2020-06-03 17:08:20 +0800

    project

---
+++ my-project/src/pages/demo/index.vue
@@ -0,0 +1,84 @@
+<template>
+    <div>
+        <input type="text" v-model="value">
+        <button @click="clicks" type="primary">add</button>
+        {{value}}
+
+        <ul>
+            <li v-for='(item,index) in arr' :key="index">
+                {{item}}
+            <button size='mini' @click='del(index)'>删除</button>    
+            </li>
+            
+        </ul>
+
+        <button  @bindtap='cli'>按钮</button>
+        <button open-type="getUserInfo" @getuserinfo="getuserinfo">按纽纽</button>
+
+        <checkbox-group @change="checkboxChange">
+          <label class="weui-cell weui-check__label" v-for="(item,index) in items" :key="index">
+            <view class="weui-cell__hd">
+              <checkbox :value="item.value" :checked="item.checked"/>
+            </view>
+            <view class="weui-cell__bd">{{item.name}}</view>
+          </label>
+        </checkbox-group>
+
+        <Hello msg='hhh' @from='msgs'></Hello>
+        {{info}}
+    </div>
+</template>
+
+<script>
+import Hello from '../index/index';
+export default {
+    data()
+    {
+        return{
+            value:'',
+            arr:[1,2,3],
+            items: [
+                {value: 'USA', name: '美国'},
+                {value: 'CHN', name: '中国', checked: 'true'},
+                {value: 'BRA', name: '巴西'},
+                {value: 'JPN', name: '日本'},
+                {value: 'ENG', name: '英国'},
+                {value: 'FRA', name: '法国'}
+            ],
+            info:''
+        }
+    },
+    methods:{
+        checkboxChange()
+        {
+
+        },
+        clicks()
+        {
+            console.log(this.value);
+            this.value='';
+        },
+        del(index)
+        {
+            this.arr.splice(index,1);
+        },
+        cli()
+        {
+            console.log("taptap");
+        },
+        msgs(infos){
+            // this.info=info;
+            console.log(infos);
+        }
+    },
+    components:{
+        Hello
+    }
+
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ my-project/src/pages/index/index.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class='b'>
+    <image class='c' src="https://dss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2364242578,2976026814&fm=26&gp=0.jpg" mode='widthFix'/>
+    <img src="https://dss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2364242578,2976026814&fm=26&gp=0.jpg" alt="">
+    <ul>
+      <li v-for="(item,index) in films" :key="index">
+        <!-- <image :src="item.poster" mode='widthFix'/> -->
+        <img :src="item.poster" alt="">
+        <image src="https://dss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2364242578,2976026814&fm=26&gp=0.jpg" mode='widthFix'/>
+        <text>{{item.name}}</text>  
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+      films:[]
+    }
+  },
+  props:['msg'],
+  components: {
+
+  },
+
+  methods: {
+    clickHandle()
+    {
+
+    }
+  },
+  mounted()
+  {
+    if(mpvuePlatform==='wx')
+    {
+      mpvue.request({
+        url:'https://www.easy-mock.com/mock/5cd62f927f8f72433eeaa0b9/api/film/getList',
+        success:(res)=>{
+            this.films=res.data.films;
+            console.log(this.films);
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style scoped lang='scss'>
+
+.b{
+  .c{
+    width:300px;
+    height: 800px;
+  }
+  img{
+    height: 900rpx;
+  }
+}
+
+</style>
